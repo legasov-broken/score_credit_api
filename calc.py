b7651619ae70b7d355a2739223c67d14b48c4417
minelove <vudinhtruongan@gmail.com>
2022-12-06 16:49:11 +0700

    finish calc func

---
+++ calc.py
@@ -0,0 +1,9 @@
+from lib import *
+from credit_score import credit_score
+
+
+
+
+input = [1,1,1,1,1,1,1,1,1,1,1,1]
+r = credit_score(input)
+print(r)
---
+++ call.py
@@ -0,0 +1,6 @@
+
+import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
